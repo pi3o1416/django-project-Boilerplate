5e56058e87af55c4aa66b5b9ce5233a48c982f39
Monir Hossain <mmonir.m.29@gmail.com>
2023-06-25 00:49:41 +0600

    Update renderer. Retrieve message from resopnse context_data.

--- core/renderers.py
+++ core/renderers.py
@@ -4,12 +4,15 @@ from rest_framework.renderers import JSONRenderer
 class CustomJSONRenderer(JSONRenderer):
     def render(self, data, accepted_media_type=None, renderer_context=None):
         custom_data = {
+            "message": None,
             "status": None,
             "data": None,
             "error": None,
         }
         response = renderer_context.get("response")
+        message = response.context_data.get('message') if type(response.context_data) is dict else None
         custom_data["status"] = response.status_code
+        custom_data["message"] = message
         if response.status_code >= 400:
             custom_data["error"] = data
         else:
